b931314003297cb940b5ff48fcad514be999cc26
yywatanabe <yuya.eth01@gmail.com>
2018-10-05 05:55:58 +0900

    adit 2018/10/8

--- tokyoIt/Sample.java
+++ tokyoIt/Sample.java
@@ -1,5 +1,18 @@
-public class Sample {
- public static void main(String[] args) {
-   System.out.println("Hello World");
-  }
+package java_std01;
+
+public class Sample4 {
+	public static void main(String[] args) {
+		//アドレス空間
+		int[] address = {1,2,3};
+		System.out.println("配列変数を出力＝" + address);
+		System.out.println("address[1]を出力＝" + address[1]);
+
+		//配列の長さを調べる
+		int[][] test3 = {{1,2},{3,4,5},{6,7,8,9}};
+		int[][] test2 = {{1,2},{3,4,5}};
+		System.out.println("test3.lengthを出力＝" + test3.length);
+		System.out.println("同上test2＝" + test2.length);
+		System.out.println("test3[2].lengthを出力＝" + test3[2].length);
+		System.out.println("同上test2＝" + test2[1].length);
+	}
 }
